967b172395c26ccb08e4dcb91ffe8ea50bcb5542
grk4 <gregkennedy0118@gmail.com>
2020-03-29 16:12:39 +0100

    adding csv into py

--- scintillation_simulation.py/project.py
+++ scintillation_simulation.py/project.py
@@ -1,4 +1,19 @@
 import csv
 
+data_list = []
+
+
 def read_data():
-    with 
+    with open("Sine 40kHz 20Vp2p 2 mic Scintillation near speaker test result.csv") as f:
+        CSVreader = csv.reader(f)
+        start_line = 8
+        count = 0
+        for row in CSVreader:
+            if count >= start_line:
+                data_list.append(row)
+            count += 1
+
+
+if __name__ == "__main__":
+    read_data()
+    print(data_list[4][1])
